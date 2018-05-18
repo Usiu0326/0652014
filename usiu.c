40e49f368f48664e3663585b330946eb1ce5d26c
鄭侑修 <usiu880326@gmail.com>
2018-05-18 22:32:33 +0800

    usiu.c

--- usiu.c
+++ usiu.c
@@ -5,31 +5,31 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int num1;
-	int num2;
+	int fitstnumber;
+	int secondnumber;
 	
 	printf("打兩個數字\n");
 	printf("答案\n");
 	
-	scanf("%d%d", &num1, &num2);
+	scanf("%d%d", &fitstnumber, &secondnumber);
 	
-	if(num1==num2){
-		printf("%d is equal to %d\n",num1,num2);
+	if(fitstnumber==secondnumber){
+		printf("%d is equal to %d\n",fitstnumber,secondnumber);
 	}
-	if(num1!=num2){
-		printf("%d is not equal to %d\n",num1,num2);
+	if(fitstnumber!=secondnumber){
+		printf("%d is not equal to %d\n",fitstnumber,secondnumber);
 	}
-	if(num1<num2){
-		printf("%d is less than %d\n",num1,num2);
+	if(fitstnumber<secondnumber){
+		printf("%d is less than %d\n",fitstnumber,secondnumber);
 	}
-	if(num1>num2){
-		printf("%d is greater than %d\n",num1,num2);
+	if(fitstnumber>secondnumber){
+		printf("%d is greater than %d\n",fitstnumber,secondnumber);
 	}
-	if(num1<=num2){
-		printf("%d is less than or equal to %d\n",num1,num2);
+	if(fitstnumber<=secondnumber){
+		printf("%d is less than or equal to %d\n",fitstnumber,secondnumber);
 	}
-	if(num1>=num2){
-		printf("%d is greater than or equal to %d\n",num1,num2);
+	if(fitstnumber>=secondnumber){
+		printf("%d is greater than or equal to %d\n",fitstnumber,secondnumber);
 	}
 	return 0;
 }
